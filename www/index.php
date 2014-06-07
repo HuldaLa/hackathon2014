c93fa6cb9112b8d36eb980895448dd59ae30cc77
func0der <funcoder@live.com>
2014-06-07 15:51:05 +0200

    Fixed index.php so that it loads the autoload correctly

--- www/index.php
+++ www/index.php
@@ -1,5 +1,7 @@
 <?php
-require 'vendor/autoload.php';
+define('DS', DIRECTORY_SEPARATOR);
+
+require '..' . DS . 'vendor' . DS . 'autoload.php';
 
 $app = new \Slim\Slim();
 $app->get('/hello/:name', function ($name) {
