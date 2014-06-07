8b94cceba32fa6e5729bb9797a734faaa7c7deed
fokoenecke <finnole.koenecke@haw-hamburg.de>
2014-06-07 18:41:52 +0200

    added bootstrap support

--- src/html/index.php
+++ src/html/index.php
@@ -4,10 +4,22 @@
 	<meta charset="utf-8" />
 	<title>App</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<script charset="utf-8" src="js/app.js"></script>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
 	<link rel="stylesheet" href="css/app.css" />
 </head>
 <body>
-<h1>Hello!</h1>
+    <h1>Hello!</h1>
+    <span class="glyphicon glyphicon-search"></span>
+    <div class="btn-group">
+        <button type="button" class="btn btn-default">Left</button>
+        <button type="button" class="btn btn-default">Middle</button>
+        <button type="button" class="btn btn-default">Right</button>
+    </div>
+
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script charset="utf-8" src="js/app.js"></script>
 </body>
 </html>
