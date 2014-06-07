3ee0f8e9cec314593253434e8c9ec15eb4492d07
mindyk <michael.indyk@googlemail.com>
2014-06-07 16:25:39 +0200

    views

--- www/index.php
+++ www/index.php
@@ -1,8 +1,28 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
+
+define ('PATH_ROOT', realpath(dirname(__FILE__)) . '/../');
+defined('PATH_WWW') or define('PATH_WWW', PATH_ROOT . 'www/');
+defined('PATH_SRC') or define('PATH_SRC', PATH_ROOT . 'src/');
+defined('PATH_VIEW') or define('PATH_VIEW', PATH_SRC . 'main/html/');
+defined('PATH_ETC') or define('PATH_ETC', PATH_ROOT . 'etc/');
+
+//+++ TEMPLATE CONFIG
+// Create new Plates engine
+$engine = new \League\Plates\Engine(PATH_VIEW);
+// Create a new template
+$template = new \League\Plates\Template($engine);
+
+//--- TEMPLATE CONFIG
 
 $app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
+
+$app->get('/', function() use ($template) {
+	$template->render('index');
+});
+
+$app->get('/hello/:name', function ($name) use ($template) {
 	echo "Hello, $name";
 });
+
 $app->run();
